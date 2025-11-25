cf7308fbbcbfecff1aa5a4486a5046204c227969
ValeriaS <vsegovia067@gmail.com>
2025-11-25 07:44:18 -0600

    Mensajitos

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ app.use(express.json()); //Esta funcion parsea el body de la peticcion y si trae
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Prueba con GET')
+  res.send('Hola Chicos! Prueba con GET')
 })
 
 app.post('/', (req, res) => {
-  res.send('Hello World! Prueba con POST')
+  res.send('Hola Chicos! Prueba con POST')
 })
 
 app.listen(port, () => {
@@ -20,20 +20,20 @@ app.listen(port, () => {
 
 app.get('/clientes', cors(), (req, res) => { //cors habilitado solo para esta ruta
   console.log(req.query);
-  res.send('Hello World! Clientes');
+  res.send('Hola Clientes! ');
 })
 
 app.get('/alumnos', (req, res) => {
   console.log(req.query);
-  res.send('Hello World! Alumnos');
+  res.send('Hola Alumnos!');
 })
 
 app.get('/docentes/:control', (req, res) => {
   console.log(req.params);
-  res.send('Hello World! Con num control');
+  res.send('Hello Docentes con numero de control!');
 })
 
 app.post('/directivos', (req, res) => {
   console.log(req.body);
-  res.send('Hello World! Directivos');
+  res.send('Hola Directivos, bienvenidos!');
 })
