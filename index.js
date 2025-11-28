f722b4942204b696ed842d6f60af1313ff1fd6b6
ValeriaS <vsegovia067@gmail.com>
2025-11-28 16:02:29 -0600

    Listo

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
 const express = require('express')
-const cors = require('cors');
+const cors = require('cors')
 const app = express()
-const port = process.env.PORT || 3000
-
-app.use(express.json()); //Esta funcion parsea el body de la peticcion y si trae JSON le agrega a php body
-app.use(cors());
-
+const mysql = require('mysql2');
+ 
+const PORT = process.env.PORT || 3000
+ 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'cafeteriadb',
+  password: 'valeria04',
+});
+ 
+app.use (express.json()); // Esta función parsea el body de la petición y si trae JSON lo agrega a req.body
+app.use(cors()) // Habilitar Cors para todas las rutas
+ 
 app.get('/', (req, res) => {
-  res.send('Hola Chicos! Prueba con GET')
-})
-
-app.post('/', (req, res) => {
-  res.send('Hola Chicos! Prueba con POST')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-app.get('/clientes', cors(), (req, res) => { //cors habilitado solo para esta ruta
+  res.send('El servidor funciona correctamente');
+});
+ 
+app.get('/clientes', cors(),(req,res)=>{ // Cors habilitado solo para esta ruta
+  // Manejo de la ruta...
   console.log(req.query);
-  res.send('Hola Clientes! ');
+  res.send('Hola Clientes!')
 })
-
+ 
 app.get('/alumnos', (req, res) => {
-  console.log(req.query);
-  res.send('Hola Alumnos!');
+  connection.query(
+    'SELECT * FROM productos WHERE IdProducto > 10',
+    function (err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+  );
 })
-
+ 
 app.get('/docentes/:control', (req, res) => {
   console.log(req.params);
-  res.send('Hello Docentes con numero de control!');
+  res.send('Hello World!')
 })
-
+ 
 app.post('/directivos', (req, res) => {
   console.log(req.body);
-  res.send('Hola Directivos, bienvenidos!');
+  res.send('Hello World!')
 })
+ 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port 3000`)
+})
+ 
+ 
